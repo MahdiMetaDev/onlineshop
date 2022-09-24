344a30b85872a2c120c396f8bcb95144d7cb56ba
mahdi <mmf1370py@gmail.com>
2022-09-24 02:43:23 -0700

    connect crispy and bootstrap

--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Crispy config
+CRISPY_TEMPLATE_PACK = 'bootstrap'
