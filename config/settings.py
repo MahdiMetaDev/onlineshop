11bcc63914807a70877e69aeecbbf76e59034fb6
mahdi <mmf1370py@gmail.com>
2022-09-30 02:04:25 -0700

    add staticfiles for frontend

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import os
 
 env = Env()
 env.read_env()
@@ -152,7 +153,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ProductPageTests(TestCase):
+
+    def test_products_list_view_url_by_name(self):
+        response = self.client.get(reverse('products_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_view_url_by_name(self):
+        response = self.client.get(reverse('product_detail', args=[1]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_products_list_page_template_used(self):
+        response = self.client.get(reverse('products_list'))
+        self.assertTemplateUsed(response, 'products/products_list.html')
+
+    def test_product_detail_page_template_used(self):
+        response = self.client.get(reverse('product_detail', args=[1]))
+        self.assertTemplateUsed(response, 'products/product_detail.html')
