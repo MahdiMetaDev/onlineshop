1abce71e5a92a9f0239434ee6d7184823ed021c2
mahdi <mmf1370py@gmail.com>
2022-10-02 06:02:50 -0700

    add custom-template-filters and admin TabularInline for panel admin

--- products/admin.py
+++ products/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
+
+
+class CommentInline(admin.TabularInline):    # we also have StackedInline
+    """
+    This is for each product comments separately
+    """
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active', ]
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', ]
+    inlines = [
+        CommentInline,
+    ]
 
 
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['product', 'author', 'body', 'stars', 'active', ]
 
