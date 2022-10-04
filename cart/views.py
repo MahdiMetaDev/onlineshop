b7097d70170c4ed69e46e73f4e2003476ef3f394
mahdi <mmf1370py@gmail.com>
2022-10-04 04:57:20 -0700

    implement add_to_cart_view and design cart_detail template

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
+from products.models import Product
 from .cart import Cart
+from .forms import AddToCartProductForm
 
 
 def cart_detail_view(request):
@@ -9,3 +11,17 @@ def cart_detail_view(request):
     return render(request, "cart/cart_detail.html", {
         'cart': cart,
     })
+
+
+def add_to_cart_view(request, product_id):
+    cart = Cart(request)
+
+    product = get_object_or_404(Product, id=product_id)
+    form = AddToCartProductForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quantity = cleaned_data['quantity']
+        cart.add(product, quantity)
+
+    return redirect('cart:cart_detail')
